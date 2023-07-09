3566e728a1e80e43f002f93f67f8ed1d9be67b1b
mustafasakalli <mustafasakalli42@gmail.com>
2023-07-09 20:35:02 +0300

    Configure sidebar and layout

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,50 @@
+"use client";
+
+import { usePathname } from "next/navigation"
+import { useMemo } from "react";
+import { BiHome, BiSearch } from "react-icons/bi"
+
 interface SidebarProps {
     children: React.ReactNode
 }
 
 const Sidebar: React.FC<SidebarProps> = ({children}) => {
-  return (
-    <div>{children}</div>
+  
+    const pathname = usePathname()
+
+    const routes = useMemo(() => [
+        {
+            icon: BiHome,
+            label: "Home",
+            active: pathname !== "/search",
+            href: "/"
+        },
+        {
+            icon: BiSearch,
+            label: "Search",
+            active: pathname === "/search",
+            href: "/search"
+        }
+    ], []);
+
+    return (
+    <div className="flex h-full">
+        <div 
+            className="
+                hidden 
+                md:flex 
+                flex-col 
+                gap-y-2
+                bg-black
+                h-full
+                w-[300px]
+                p-2">
+            <Box>
+                "Sidebar navigator"
+            </Box>
+        </div>
+    </div>
   )
 }
 
-export default Sidebar
+export default Sidebar;
