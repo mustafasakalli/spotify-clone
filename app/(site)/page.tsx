d8574c7c2521ca60799e5cf688314e1d5fb0a1d8
mustafasakalli <mustafasakalli42@gmail.com>
2023-07-30 10:54:18 +0300

    page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <div className="text-green-500">
-      main content
+    <div className="
+      bg-neutral-900
+      rounded-lg
+      overflow-hidden
+      overflow-y-auto
+      h-full
+      w-full
+    ">
+      
     </div>    
   )
 }
